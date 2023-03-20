d19c16bf2f5e69a411e33cd7e1b5c34184fbb44e
homew01\steven <steven.tardonia@outlook.com>
2023-03-20 10:33:42 -0400

    Finished new data layer.

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
 from flask import Flask
+from data import jokes
 import random
 
 app = Flask(__name__)
 
-jokes = [
-    "Relationship status? I'll leave the relations to the database.",
-    "How do you get the code for the bank vault? You checkout their branch.",
-    "Why did the security conscious engineer refuse to pay their dinner bill? Because they could not verify the checksum.",
-    "How many Prolog programmers does it take to change a lightbulb? Yes."
-]
+
 @app.get('/')
 def tell_a_joke():
     joke = random.choice(jokes)
---
+++ data.py
@@ -0,0 +1,6 @@
+jokes = [
+    "Relationship status? I'll leave the relations to the database.",
+    "How do you get the code for the bank vault? You checkout their branch.",
+    "Why did the security conscious engineer refuse to pay their dinner bill? Because they could not verify the checksum.",
+    "How many Prolog programmers does it take to change a lightbulb? Yes."
+]
