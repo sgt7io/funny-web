37a63d31bc7df3c3054c85423a217a9b794dc52d
homew01\steven <steven.tardonia@outlook.com>
2023-03-22 09:11:41 -0400

    Moved to data namespace.

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import flask
 from flask import Flask
-from data import jokes
+import data
 import random
 
 app = Flask(__name__)
 
 @app.get('/')
 def tell_a_joke():
-    joke = random.choice(jokes)
+    joke = random.choice(data.jokes)
     return flask.render_template('joke.html', joke_text=joke)
